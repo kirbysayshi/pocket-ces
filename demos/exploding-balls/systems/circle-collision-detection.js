e086d5300dda514281010d541c68173d94f6334f
Andrew Petersen <kirbysayshi@gmail.com>
2014-06-04 00:04:49 -0400

    fix collision detection

--- demos/exploding-balls/systems/circle-collision-detection.js
+++ demos/exploding-balls/systems/circle-collision-detection.js
@@ -2,25 +2,26 @@ exports.name = 'circle-collision-detection';
 exports.reqs = ['radius', 'verlet-position', 'collision-information', 'collidable-group-a'];
 exports.action = function action(pkt, entities, circles, positions, detectionData) {
   var i, j, entityA, entityB, circleA, circleB, positionA, positionB, dataA, dataB;
+
+  // Reset... super inefficient.
+  for(i = 0; i < entities.length; i++) {
+    entityA = entities[i];
+    dataA = detectionData[entityA.id];
+    dataA.isColliding = false;
+    dataA.collidingWith = [];
+  }
+
   for(i = 0; i < entities.length; i++) {
     entityA = entities[i];
     circleA = circles[entityA.id];
     positionA = positions[entityA.id];
     dataA = detectionData[entityA.id];
 
-    // Reset...
-    dataA.isColliding = false;
-    dataA.collidingWith = [];
-
     for(j = i+1; j < entities.length; j++) {
       entityB = entities[j];
       circleB = circles[entityB.id];
       positionB = positions[entityB.id];
       dataB = detectionData[entityB.id];
-
-      // Reset...
-      dataB.isColliding = false;
-      dataB.collidingWith = [];
 
       if (
         circleOverlap(positionA.cpos.x, positionA.cpos.y, circleA.rad,
